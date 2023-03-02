cbe596ce0aea21b52f365f42e63c9eeead2662d1
Bjørn Kristian Punsvik <bjorn.kristian.punsvik@tietoevry.com>
2023-03-02 08:18:32 +0100

    Add slack service.
    With minimal testing.

---
+++ node-ts/src/services/slack.service.spec.ts
@@ -0,0 +1,16 @@
+﻿import {config} from "dotenv";
+config({ path: '../.env' });
+import {SlackService} from "./slack.service";
+
+describe('init', () => {
+    test('can initiate service', async () => {
+        new SlackService()
+    })
+})
+
+describe('message', () => {
+    test('can send message', async () => {
+        const slack = new SlackService()
+        await slack.message('Bjørn Kristian Punsvik', 'Super content', 'https://tietoevry.com/')
+    })
+})
